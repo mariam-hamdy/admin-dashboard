1b1439b71d5f0d6229ec709078c8c5d81c215d22
mariam-hamdy <62889447+mariam-hamdy@users.noreply.github.com>
2022-07-13 16:00:56 +0200

    Add files via upload

--- src/app/components/user-create/user-create.component.ts
+++ src/app/components/user-create/user-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { IUser } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-create',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService, private snackBar: MatSnackBar, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+
+  addNewUser(addUserForm: NgForm) {
+
+    if (addUserForm.valid) {
+      let user: IUser = {
+        id: 0,
+        name: addUserForm.controls['username']?.value,
+        phone: addUserForm.controls['phone']?.value,
+        email: addUserForm.controls['email']?.value,
+        website: addUserForm.controls['website']?.value
+      }
+      this.userService.addUser(user);
+      this.snackBar.open("Added Successfully", "X",{
+        duration: 3000
+      });
+      this.router.navigate(['/users']);
+
+    }
+  }
 }
